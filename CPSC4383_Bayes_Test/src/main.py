b172ac22c766a3fa9bb7ee2fc5a81a0a72218555
C-Smith-UALR <clarklander1983@gmail.com>
2019-09-15 16:49:12 -0500

    second commit

--- CPSC4383_Bayes_Test/src/main.py
+++ CPSC4383_Bayes_Test/src/main.py
@@ -2,26 +2,113 @@
 #Clark L. Smith
 #September 12, 2019
 #A program that uses Naive Bayes to predict conditional probability of developing diabetes. 
-
+#source code: https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
 import csv
 import random
-import os
+import math
 
 def loadCsv(filename):
-    lines = csv.reader(open(filename, "r"))
+    lines = csv.reader(open(filename, "rb"))
     dataset = list(lines)
     for i in range(len(dataset)):
         dataset[i] = [float(x) for x in dataset[i]]
-        return dataset
+    return dataset
 
-filename = '/Users/The_Don_Account/eclipse-workspace/CPSC4383_Bayes_Test/pima-indians-diabetes.data.csv'
-dataset = loadCsv(filename)
-print('Loaded data file {0} with {1} rows').format(filename, len(dataset))  
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
 
-#dir_path = os.path.dirname(os.path.realpath(__file__))  
-#print(dir_path)
+#creates a new list with attributes grouped with class value
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
 
-myInt=66
-print(myInt)
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
 
-#this is a change
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    return math.sqrt(variance)
+
+#this function will create a list of lists, one list for each attribute, the elements of which are mean and stdev
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.iteritems():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+##### MAKING A PREDICTION ##########
+# class values for the dataset: 0, 1.  Attribute values: age, lab, pregnancies
+
+#this function calculates the conditional probability of an attribute belonging to the class
+#using the PDF of a normal distribution
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+
+#this function will return the conditional probabilities of a value x
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.iteritems():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+#this function returns the class with the highest probability            
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.iteritems():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct += 1
+    return (correct/float(len(testSet))) * 100.0
+
+def main():
+    filename = '/Users/The_Don_Account/git/CPSC4383_Bayes_Test/CPSC4383_Bayes_Test/pima-indians-diabetes.data.csv'
+    splitRatio = 0.67
+    dataset = loadCsv(filename)
+    trainingSet, testSet = splitDataset(dataset, splitRatio)
+    print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
+    # prepare model
+    summaries = summarizeByClass(trainingSet)
+    # test model
+    predictions = getPredictions(summaries, testSet)
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: {0}%').format(accuracy)
+
+main()
